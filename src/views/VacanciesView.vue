89f565de7d13e80dac4f342d7832328ef238a3c1
Viktor Dzhurliak <dvictor74@gmail.com>
2026-04-03 14:34:37 +0300

    Refactore menu

--- src/views/VacanciesView.vue
+++ src/views/VacanciesView.vue
@@ -11,11 +11,5 @@
 </template>
 
 <script setup>
-//import HeroSection from '@/components/HeroSection.vue'
-//import AboutSection from '@/components/AboutSection.vue'
-//import ServicesSection from '@/components/ServicesSection.vue'
-//import ProjectsSection from '@/components/ProjectsSection.vue'
-//import NewsSection from '@/components/NewsSection.vue'
-import VacanciesSection from '@/components/VacanciesSection.vue'
-/*import ContactsSection from '@/components/ContactsSection.vue' */
+  import VacanciesSection from '@/components/VacanciesSection.vue'
 </script>
