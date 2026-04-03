a8f2d865c6fef7af4445d1912547ec9f8ca58fef
Viktor Dzhurliak <dvictor74@gmail.com>
2026-04-03 12:20:17 +0300

    Commit all

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import NewsView from '@/views/NewsView.vue'
+import VacanciesView from '@/views/VacanciesView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ServicesView from '@/views/ServicesView.vue'
+import ProjectsView from '@/views/ProjectsView.vue'
+import ContactsView from '@/views/ContactsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +16,36 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: ServicesView,
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsView,
+    },
+    {
       path: '/news/:id',
       name: 'news-detail',
-      component: () => import('@/views/NewsDetailView.vue'),
+      //component: () => import('@/views/NewsDetailView.vue'),
+      component: NewsView,
     },
     {
       path: '/vacancies/:id',
       name: 'vacancy-detail',
-      component: () => import('@/views/VacancyDetailView.vue'),
+      //component: () => import('@/views/VacancyDetailView.vue'),
+      component: VacanciesView,
+    },
+    {
+      path: '/contacts',
+      name: 'contacts',
+      component: ContactsView,
     },
   ],
   scrollBehavior(to, from, savedPosition) {
